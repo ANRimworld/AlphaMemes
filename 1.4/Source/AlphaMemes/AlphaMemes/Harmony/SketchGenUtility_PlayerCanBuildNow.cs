0bff263db111ec515e87e81dcb5e333caec80cd1
ANRimworld <allie.noble.an@gmail.com>
2022-10-13 17:00:55 -0700

    Fix for monuments requiring things not buildable

--- 1.4/Source/AlphaMemes/AlphaMemes/Harmony/SketchGenUtility_PlayerCanBuildNow.cs
+++ 1.4/Source/AlphaMemes/AlphaMemes/Harmony/SketchGenUtility_PlayerCanBuildNow.cs
@@ -2,6 +2,7 @@
 using RimWorld;
 using RimWorld.SketchGen;
 using Verse;
+using VanillaMemesExpanded;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,13 +24,20 @@ namespace AlphaMemes
 
             if (StaticCollectionsClass.designatorsToBeRemoved.Contains(buildable.designatorDropdown))
             {
-
-
                 __result = false;
-
-
             }
-
+            //Adding to prevent monuments from adding things that players can't build
+            foreach (var ideo in Faction.OfPlayer.ideos.AllIdeos)
+            {
+                foreach (var meme in ideo.memes)
+                {
+                    var ext = meme.GetModExtension<ExtendedMemeProperties>();
+                    if (ext!=null && ext.removedDesignators.Any(x=>x == (buildable as ThingDef)))
+                    {
+                        __result = false;
+                    }
+                }
+            }
 
 
 
